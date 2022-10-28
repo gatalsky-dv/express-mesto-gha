8c5992f388cdbe3f8062c2bb3f73ee5bf2382f33
unknown <C:\Users\\AppData\Roaming\The Bat!>
2022-10-28 22:50:35 +0500

    bugfix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new Unauthorized('Необходима авторизация'));
   }
-  const token = authorization.replace('Bearer ', '');
+  const token = String(req.headers.authorization).replace('Bearer ', '');
   let payload;
 
   try {
