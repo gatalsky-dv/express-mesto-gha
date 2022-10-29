b21553a1087ac7006ee0ec7e20e860c4b1a892ce
unknown <C:\Users\\AppData\Roaming\The Bat!>
2022-10-29 13:50:51 +0500

    bugfix users controllers getUserMe

--- controllers/users.js
+++ controllers/users.js
@@ -29,24 +29,34 @@ module.exports.getUser = (req, res, next) => {
 module.exports.getUserMe = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => {
-      if (!user._id) {
-        throw new NotFound('Пользователь не найден');
-      }
-      res.send({
-        _id: user._id,
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-        email: user.email,
-      });
+      if (user) {
+        res.status(200).send({
+          _id: user._id,
+          name: user.name,
+          about: user.about,
+          avatar: user.avatar,
+          email: user.email,
+        });
+      } else { throw new BadRequest('Переданы некорректные данные'); }
+      // if (!user._id) {
+      //   throw new NotFound('Пользователь не найден');
+      // }
+      // res.send({
+      //   _id: user._id,
+      //   name: user.name,
+      //   about: user.about,
+      //   avatar: user.avatar,
+      //   email: user.email,
+      // });
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Переданы некорректные данные'));
-      } else {
-        next(err);
-      }
-    });
+    .catch(next);
+  // .catch((err) => {
+  //   if (err.name === 'CastError') {
+  //     next(new BadRequest('Переданы некорректные данные'));
+  //   } else {
+  //     next(err);
+  //   }
+  // });
 };
 
 module.exports.createUser = (req, res, next) => {
